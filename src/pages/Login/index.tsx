b5925677e97f06e6d5438b8737f10c5bac3b1e47
smp6340 <smp6340@naver.com>
2023-02-10 21:58:59 +0900

    ✨ :: Login complete

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,10 +1,25 @@
 import React, { useState, Component } from "react";
-import axios from "axios";
+import { Route } from "react-router-dom";
+import axios, { AxiosResponse } from "axios";
 
 // Styles
 import * as S from "./style";
 
 function Login(): React.ReactElement {
+    const [accountId, setAccountId] = useState("");
+    const [password, setPassword] = useState("");
+    async function Login() {
+        axios.post("https://todayisdiary.site/user/login", {
+            accountId : accountId,
+            password : password,
+        })
+        .then((res) => {
+            console.log(res);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    }
     return(
         <>
             <S.Page>
@@ -13,10 +28,20 @@ function Login(): React.ReactElement {
                         <S.LoginTitle>관리자 로그인</S.LoginTitle>
                     </S.TitleWrapper>
                     <S.InputWrapper>
-                        <S.IdInput type={"text"} placeholder="아이디" />
-                        <S.PasswordInput type={"password"} placeholder="비밀번호" />
+                        <S.IdInput
+                            type={"text"} 
+                            value={accountId} 
+                            onChange={(e) => setAccountId(e.target.value)}
+                            placeholder="아이디"
+                        />
+                        <S.PasswordInput 
+                            type={"password"}
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)} 
+                            placeholder="비밀번호" 
+                        />
                     </S.InputWrapper>
-                    <S.SubmitBtn>로그인</S.SubmitBtn>
+                    <S.SubmitBtn onClick={() => Login()}>로그인</S.SubmitBtn>
                 </S.LoginWrapper>
                 <S.LogoWrapper>
                     <S.LogoImg src="https://cdn.discordapp.com/attachments/831153482911973398/1049644059188277278/Moon.png" />
