24880b6b0176731a8e7f1f46a4db924304549f95
smp6340 <smp6340@naver.com>
2023-03-14 19:08:43 +0900

    fix :: 라우팅 재설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom';
 
 // Pages
 import Login from './pages/Login';
+import Diary from './pages/report/diary';
 
 function App() {
   return (
     <>
-      <Login />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/report/diary' element={<Diary />} />
+        </Routes>
+      </Router>
     </>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import { BrowserRouter as Router } from 'react-router-dom';
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <Router>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Router>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
